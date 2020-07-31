de57c53126ccc07d1d7ce43feddf3f7e6eb03cc1
PratthamArora <arora.prattham@gmail.com>
2020-07-31 21:44:46 +0530

    define drop down button

--- lib/screens/note_detail.dart
+++ lib/screens/note_detail.dart
@@ -1,1 +1,48 @@
+import 'package:flutter/material.dart';
 
+class NoteDetail extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return NoteDetailState();
+  }
+}
+
+class NoteDetailState extends State<NoteDetail> {
+  static var _priority = ['High', 'Low'];
+
+  @override
+  Widget build(BuildContext context) {
+    TextStyle textStyle = Theme.of(context).textTheme.title;
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Edit Note'),
+        ),
+        body: Padding(
+          padding: EdgeInsets.only(top: 15.0, left: 10.0, right: 10.0),
+          child: ListView(
+            children: <Widget>[
+              // First element
+              ListTile(
+                title: DropdownButton(
+                  items: _priority.map((String dropDownStringItem) {
+                    return DropdownMenuItem<String>(
+                      value: dropDownStringItem,
+                      child: Text(dropDownStringItem),
+                    );
+                  }).toList(),
+                  style: textStyle,
+                  value: 'Low',
+                  onChanged: (valueSelectedByUser) {
+                    setState(() {
+                      debugPrint('User selected: $valueSelectedByUser');
+                    });
+                  },
+                ),
+              )
+            ],
+          ),
+        ));
+    ;
+  }
+}
