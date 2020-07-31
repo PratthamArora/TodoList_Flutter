d0cda32a32a7bd8496ebd8c2ffefc10b5dffe82b
PratthamArora <arora.prattham@gmail.com>
2020-07-31 21:34:17 +0530

    added func to display all notes

--- lib/screens/note_list.dart
+++ lib/screens/note_list.dart
@@ -8,13 +8,52 @@ class NoteList extends StatefulWidget {
 }
 
 class NoteListState extends State<NoteList> {
+  int count = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Notes'),
       ),
-      body: Text('body'),
+      body: getNotes(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          debugPrint("FAB Tapped");
+        },
+        tooltip: 'Add Note',
+        child: Icon(Icons.add),
+      ),
     );
+  }
+
+  ListView getNotes() {
+    TextStyle titleStyle = Theme.of(context).textTheme.subhead;
+    return ListView.builder(
+        itemCount: count,
+        itemBuilder: (BuildContext context, int pos) {
+          return Card(
+            color: Colors.white,
+            elevation: 2.0,
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.yellow,
+                child: Icon(Icons.keyboard_arrow_right),
+              ),
+              title: Text(
+                'Dummy Title',
+                style: titleStyle,
+              ),
+              subtitle: Text('Dummy Date'),
+              trailing: Icon(
+                Icons.delete,
+                color: Colors.grey,
+              ),
+              onTap: () {
+                debugPrint("ListTile Tapped");
+              },
+            ),
+          );
+        });
   }
 }
